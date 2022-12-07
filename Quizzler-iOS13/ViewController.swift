eb6f47f637a2f3ab2b22ce1c2b2217335729fad2
Tomas Padilla <padillatomasagustin@gmail.com>
2022-12-07 08:02:56 +0100

    [MVCQuizzlet] Main structure OK - Will change Array to Struct

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    
+    let questions = [
+        ["2 + 4 is equal to 6", "True"],
+        ["5 - 3 is greater than 1", "True"],
+        ["3 + 8 is less than 10", "False"],
+    ]
+    
+    var currQuestionIndex = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
-
+    
+    
+    @IBAction func answerBtnPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = questions[currQuestionIndex][1]
+        
+        if userAnswer == actualAnswer {
+            print("Correct")
+        } else {
+            print("Oops!")
+        }
+        
+        // Next question + RefreshUI
+        if currQuestionIndex + 1 < questions.count {
+            currQuestionIndex += 1
+        } else {
+            currQuestionIndex = 0
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = questions[currQuestionIndex][0]
+    }
+    
 
 }
 
